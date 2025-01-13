1e6d0ea411aa12d246be45ad2a24b3f3b5f6ae75
Caio Sendin <caiosendindev.com>
2025-01-13 18:38:43 -0300

    feat: add question listing controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate.controller'
-import { CreateAccountController } from './controllers/create-account.controller'
-import { CreateQuestionController } from './controllers/create-question.controller'
+import { CreateQuestionController } from './controllers/questions/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/questions/fetch-recent-questions.controller'
+import { AuthenticateController } from './controllers/users/authenticate.controller'
+import { CreateAccountController } from './controllers/users/create-account.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
@@ -16,7 +17,12 @@ import { PrismaService } from './prisma/prisma.service'
         }),
         AuthModule,
     ],
-    controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
+    controllers: [
+        CreateAccountController,
+        AuthenticateController,
+        CreateQuestionController,
+        FetchRecentQuestionsController,
+    ],
     providers: [PrismaService],
 })
 export class AppModule {}
